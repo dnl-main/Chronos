cc0425d2a46441b9780f74614ec2a7d79c1f7e5b
Okami <kennethsianghio756@gmail.com>
2025-05-17 16:23:10 +0800

    new branch

--- JWTbackweb/config/cors.php
+++ JWTbackweb/config/cors.php
@@ -3,9 +3,9 @@
 return [
 
     /*
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------
     | Laravel CORS Settings
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------
     |
     | Here you can configure your CORS settings for your application. By default, 
     | Laravel uses the `fruitcake/laravel-cors` package, but if you are using 
@@ -14,9 +14,9 @@ return [
     */
 
     /*
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------
     | Allowed Paths
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------
     |
     | You can specify which paths should be included in the CORS handling.
     | In this case, all API routes are handled (for example: api/*).
@@ -26,39 +26,35 @@ return [
     'paths' => ['api/*'],  // Apply CORS settings to all API routes.
 
     /*
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------
     | Allowed Methods
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------
     |
-    | Specify which HTTP methods are allowed. Use ['*'] to allow all methods.
+    | Specify which HTTP methods are allowed. Use specific methods instead of ['*']
+    | when credentials are supported.
     |
     */
 
-    'allowed_methods' => ['*'], // Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], // Explicitly allow common methods
 
     /*
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------
     | Allowed Origins
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------
     |
     | List the origins that are allowed to make requests to your API. 
     | Replace `http://localhost:5173` with your front-end URL.
-
-    add ngrok url 
     |
     */
 
     'allowed_origins' => [
-    'http://112.203.155.181',
-    'http://localhost:5173',
-    'https://concorde-web.vercel.app',
-    'https://heroic-sensible-buzzard.ngrok-free.app',
+        'https://concorde-web.vercel.app',
     ],
 
     /*
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------
     | Allowed Origins Patterns
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------
     |
     | You can also use regular expressions to match multiple origins.
     | Leave this empty if you're not using it.
@@ -68,21 +64,21 @@ return [
     'allowed_origins_patterns' => [],
 
     /*
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------
     | Allowed Headers
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------
     |
-    | Specify which headers are allowed in the request. By default, all headers
-    | are allowed with ['*'].
+    | Specify which headers are allowed in the request. Include Authorization
+    | for JWT tokens and other common headers.
     |
     */
 
-    'allowed_headers' => ['*'],  // Allow all headers
+    'allowed_headers' => ['Content-Type', 'Accept', 'Authorization', 'X-Requested-With'],  // Explicitly allow specific headers
 
     /*
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------
     | Exposed Headers
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------
     |
     | Specify which headers are allowed to be exposed in the response.
     |
@@ -91,9 +87,9 @@ return [
     'exposed_headers' => [],  // Leave empty for no exposed headers.
 
     /*
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------
     | Max Age
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------
     |
     | Define how long the results of the pre-flight request can be cached.
     |
@@ -102,14 +98,14 @@ return [
     'max_age' => 0,  // Cache pre-flight responses for 0 seconds.
 
     /*
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------
     | Supports Credentials
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------
     |
     | By default, credentials (cookies, HTTP authentication) are not included.
     | Set this to `true` if you want to allow credentials in the request.
     |
     */
 
-    'supports_credentials' => true,  // Disable credentials (cookies, HTTP authentication)
+    'supports_credentials' => true,  // Enable credentials (cookies, HTTP authentication)
 ];
